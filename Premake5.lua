dee799b02a138bc1e3a2896f8ea4a937476c40e9
MrMoonshine26 <pratap.delhi2006@gmail.com>
2024-05-22 12:15:34 +0530

    Adding sqlite-cpp

---
+++ Premake5.lua
@@ -0,0 +1,54 @@
+project "SQLite-CPP"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include",
+        "sqlite3"
+	}
+
+	defines
+	{
+		"SQLITE_OMIT_LOAD_EXTENSION"
+	}
+ 
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+    
+    filter "system:macosx"
+		cppdialect "c++20"
+		staticruntime "On"
+    
+        links{
+            "pthread",
+            "dl"
+        }
+
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
